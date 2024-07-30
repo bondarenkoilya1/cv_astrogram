d95bba1c8b9f734ac411e489d4ea2f65560b0f76
Ilya <bondarenkoilya2@gmail.com>
2024-07-30 17:52:16 +0300

    add fonts, connect them all

--- src/styled.js
+++ src/styled.js
@@ -4,21 +4,28 @@ import { css } from "@emotion/react";
 
 import styled from "@emotion/styled";
 
+import { connectFonts } from "./fonts";
+
 export const GlobalStyle = css`
-	${emotionNormalize}
+  ${emotionNormalize}
+  ${connectFonts}
 	
 	html {
-		box-sizing: border-box;
-	}
-	
-	*,
-	*::before,
-	*::after {
-		box-sizing: inherit;
-		margin: 0;
-		padding: 0;
-	}
-}`;
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;
+  }
+
+  #root {
+    font-family: "SF Pro Text", sans-serif;
+  }
+`;
 
 export const ContainerStyled = styled.div`
   max-width: 1520px;
