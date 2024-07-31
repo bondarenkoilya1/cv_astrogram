328a571954197e0ef54658453c94899ca0048995
Ilya <bondarenkoilya2@gmail.com>
2024-07-31 22:02:39 +0300

    improve radio group

--- src/components/ui/RadioGroup/RadioGroup.jsx
+++ src/components/ui/RadioGroup/RadioGroup.jsx
@@ -1,26 +1,37 @@
 import PropTypes from "prop-types";
 
+import { RadioGroupListStyled, RadioGroupStyled, RadioGroupTitleStyled } from "./styled";
+
 import { RadioButton } from "../RadioButton";
 
-export const RadioGroup = ({ array, isChecked, setIsChecked }) => {
+export const RadioGroup = ({ array, isChecked, setIsChecked, title }) => {
   return (
-    <ul>
-      {array &&
-        array.map(({ name, text }) => (
-          <RadioButton
-            name={name}
-            isChecked={isChecked}
-            setIsChecked={setIsChecked}
-            key={crypto.randomUUID()}>
-            {text}
-          </RadioButton>
-        ))}
-    </ul>
+    <RadioGroupStyled>
+      {title && <RadioGroupTitleStyled>{title}</RadioGroupTitleStyled>}
+      <RadioGroupListStyled>
+        {array &&
+          array.map(({ name, text }) => (
+            <RadioButton
+              name={name}
+              isChecked={isChecked}
+              setIsChecked={setIsChecked}
+              key={crypto.randomUUID()}
+              style={{ marginRight: "10px" }}>
+              {text}
+            </RadioButton>
+          ))}
+      </RadioGroupListStyled>
+    </RadioGroupStyled>
   );
 };
 
 RadioGroup.propTypes = {
   array: PropTypes.array.isRequired,
   isChecked: PropTypes.bool.isRequired,
-  setIsChecked: PropTypes.func.isRequired
+  setIsChecked: PropTypes.func.isRequired,
+  title: PropTypes.string
+};
+
+RadioGroup.defaultProps = {
+  title: ""
 };
---
+++ src/components/ui/RadioGroup/styled.js
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const RadioGroupStyled = styled.div``;
+
+export const RadioGroupTitleStyled = styled.h4`
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 26px;
+  margin-bottom: 15px;
+`;
+
+export const RadioGroupListStyled = styled.ul`
+  display: flex;
+  align-items: center;
+`;
