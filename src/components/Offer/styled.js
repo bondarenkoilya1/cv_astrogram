e4adc32aa54d2c9cc7241d1245f419f2c193c22c
Ilya <bondarenkoilya2@gmail.com>
2024-08-03 01:30:34 +0300

    create offer card

---
+++ src/components/Offer/styled.js
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+
+export const OfferStyled = styled.section``;
+
+export const OfferTitleStyled = styled.h2`
+  text-transform: uppercase;
+  max-width: 941px;
+  margin: 60px auto 0 auto;
+  font-family: "Foglihten", serif;
+  font-size: 40px;
+  font-weight: 500;
+  line-height: 48px;
+  text-align: center;
+`;
+
+export const OfferDescriptionStyled = styled.p`
+  max-width: 613px;
+  margin: 20px auto 0 auto;
+  text-align: center;
+  font-size: 16px;
+  line-height: 26px;
+`;
+
+export const OfferLinkContainerStyled = styled.div`
+  margin-top: 44px;
+  display: flex;
+  justify-content: center;
+`;
