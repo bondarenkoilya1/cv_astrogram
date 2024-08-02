61f97a16de5ab554a721acd40fb39b648f9d35e3
Ilya <bondarenkoilya2@gmail.com>
2024-08-02 19:20:45 +0300

    create another component with slider

--- src/data/products.js
+++ src/data/products.js
@@ -1,3 +1,7 @@
+import couple from "../assets/images/products/couple.png";
+import coupleInHeart from "../assets/images/products/couple-in-heart.png";
+import heart from "../assets/images/products/heart.png";
+import money from "../assets/images/products/money.png";
 import soap from "../assets/images/products/soap.png";
 
 export const productsList = [
@@ -103,6 +107,108 @@ export const productsList = [
     imageSrc: soap,
     category: "Свечи",
     title: "Свеча серии «Бахрейн» Свеча серии много текста в 3 ряда для карточки товара",
+    currentPrice: 290,
+    oldPrice: 290
+  }
+];
+
+export const otherProductsList = [
+  {
+    imageSrc: heart,
+    category: "Любовь",
+    title: "Как звезды влияют на поиск своего счастья",
+    currentPrice: 290,
+    oldPrice: 290
+  },
+  {
+    imageSrc: money,
+    category: "Деньги",
+    title: "Как звезды притягивают деньги в вашу жизнь",
+    currentPrice: 290,
+    oldPrice: 290
+  },
+  {
+    imageSrc: coupleInHeart,
+    category: "Любовь",
+    title: "Любовная совместимость для знаков зодиака"
+  },
+  {
+    imageSrc: couple,
+    category: "Любовь",
+    title: "Когда я выйду замуж? Гороскоп замужества",
+    currentPrice: 290,
+    oldPrice: 290
+  },
+  {
+    imageSrc: money,
+    category: "Деньги",
+    title: "Как звезды притягивают деньги в вашу жизнь",
+    currentPrice: 290,
+    oldPrice: 290
+  },
+  {
+    imageSrc: heart,
+    category: "Любовь",
+    title: "Как звезды влияют на поиск своего счастья",
+    currentPrice: 290,
+    oldPrice: 290
+  },
+  {
+    imageSrc: money,
+    category: "Деньги",
+    title: "Как звезды притягивают деньги в вашу жизнь",
+    currentPrice: 290,
+    oldPrice: 290
+  },
+  {
+    imageSrc: coupleInHeart,
+    category: "Любовь",
+    title: "Любовная совместимость для знаков зодиака"
+  },
+  {
+    imageSrc: couple,
+    category: "Любовь",
+    title: "Когда я выйду замуж? Гороскоп замужества",
+    currentPrice: 290,
+    oldPrice: 290
+  },
+  {
+    imageSrc: money,
+    category: "Деньги",
+    title: "Как звезды притягивают деньги в вашу жизнь",
+    currentPrice: 290,
+    oldPrice: 290
+  },
+  {
+    imageSrc: heart,
+    category: "Любовь",
+    title: "Как звезды влияют на поиск своего счастья",
+    currentPrice: 290,
+    oldPrice: 290
+  },
+  {
+    imageSrc: money,
+    category: "Деньги",
+    title: "Как звезды притягивают деньги в вашу жизнь",
+    currentPrice: 290,
+    oldPrice: 290
+  },
+  {
+    imageSrc: coupleInHeart,
+    category: "Любовь",
+    title: "Любовная совместимость для знаков зодиака"
+  },
+  {
+    imageSrc: couple,
+    category: "Любовь",
+    title: "Когда я выйду замуж? Гороскоп замужества",
+    currentPrice: 290,
+    oldPrice: 290
+  },
+  {
+    imageSrc: money,
+    category: "Деньги",
+    title: "Как звезды притягивают деньги в вашу жизнь",
     currentPrice: 290,
     oldPrice: 290
   }
