f7d334707c63f5c8f9a1c402a0e957dc525e979d
Ilya <bondarenkoilya2@gmail.com>
2024-08-03 11:31:03 +0300

    update offer component

--- src/components/Offer/Offer.jsx
+++ src/components/Offer/Offer.jsx
@@ -9,14 +9,14 @@ import {
 
 import { Image, Link } from "../ui";
 
-export const Offer = ({ imageSrc, imageAlt, title, description, ...attrs }) => {
+export const Offer = ({ imageSrc, imageAlt, title, description, linkText, ...attrs }) => {
   return (
     <OfferStyled {...attrs}>
       <Image src={imageSrc} alt={imageAlt} height="560px" width="100%" />
       <OfferTitleStyled>{title}</OfferTitleStyled>
       <OfferDescriptionStyled>{description}</OfferDescriptionStyled>
       <OfferLinkContainerStyled>
-        <Link smallPadding>Узнать совместимость</Link>
+        <Link smallPadding>{linkText}</Link>
       </OfferLinkContainerStyled>
     </OfferStyled>
   );
@@ -26,5 +26,6 @@ Offer.propTypes = {
   imageSrc: PropTypes.string.isRequired,
   imageAlt: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
+  description: PropTypes.string.isRequired,
+  linkText: PropTypes.string.isRequired
 };
