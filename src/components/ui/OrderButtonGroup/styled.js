685725d649d49163c2be0a6c7996f3b51a5da9db
Ilya <bondarenkoilya2@gmail.com>
2024-08-04 16:30:39 +0300

    update order button group styles

--- src/components/ui/OrderButtonGroup/styled.js
+++ src/components/ui/OrderButtonGroup/styled.js
@@ -3,4 +3,7 @@ import styled from "@emotion/styled";
 export const OrderButtonGroupStyled = styled.ul`
   display: flex;
   align-items: center;
+  margin-top: 60px;
+  justify-content: center;
+  margin-bottom: 160px;
 `;
